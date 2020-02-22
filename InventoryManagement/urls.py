3d3d6491ded43731f400aa827a9ff3e384a46983
Supreet Singh <supreet51.cs@gmail.com>
2020-02-22 16:06:58 +0530

    merge conflict reso;

--- InventoryManagement/urls.py
+++ InventoryManagement/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
